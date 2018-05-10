498cb2d2b1fdf9d122611370f9dd32faf264ce2d
wingardtw <wingardtw94@gmail.com>
2018-05-10 13:23:05 -0700

    removed comments

--- authentication/forms.py
+++ authentication/forms.py
@@ -8,21 +8,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 
-
-#class UserForm():
-   # password = forms.CharField(widget=forms.PasswordInput())
-
-    #class Meta():
-   #     model=get_user_model()
-   #     fields = ('username','email','password1','password2')
-
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length = 254, required = True, widget = forms.EmailInput(), help_text='Required. Please provide a valid email address.')
     class Meta:
         fields = ("username", "email", "password1", "password2")
         model = User
-
-   # def __init__(self, *args, **kwargs):
-       # super().__init__(*args, **kwargs)
-        #self.fields["username"].label = "Display name"
-       # self.fields["email"].label = "Email address"
