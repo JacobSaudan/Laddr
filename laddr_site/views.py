112488c68de8f50e02b1b48722af23574518f591
Taylor Wingard <twingard@raybeam.com>
2018-05-31 20:29:10 -0700

    team view start

--- laddr_site/views.py
+++ laddr_site/views.py
@@ -57,26 +57,37 @@ def patch_notes(request):
 	return render(request, html)
 
 def player_card_data(request):
-	user_id = request.GET.get('user_id', 1)
+	user_id = request.data.get('user_id', None)
+	if user_id == None:
+		return {
+			"details": None,
+			"error": "Expected user_id in request, got None instead",
+			"success": False,
+		}
 	player_card_data = get_player_card(user_id)
 	return JsonResponse(player_card_data)
 
 def get_player_card(player_id):
-	user_id = request.GET.get('user_id', 1)
-	user = User.objects.get(id=user_id)
+	user = User.objects.get(id=player_id)
 	profile = Profile.objects.get(user=user)
 	player_card_data = {
-		'user_name': user.username,
-		'summoner_name': profile.summoner_name,
-		'server': profile.lol_server,
-		'top_champions': profile.top_champions,
 		'bio': profile.bio,
+		'header_color': profile.favorite_color,
+		'psyche': profile.get_psyche(),
 		'rank': profile.rank,
 		'role': profile.role,
-		'psyche': profile.get_psyche(),
-		'header_color': profile.favorite_color,
+		'server': profile.lol_server,
+		'summoner_name': profile.summoner_name,
+		'top_champions': profile.top_champions,
+		'user_name': user.username,
 	}
-	return player_card_data
+	return {
+		"success": True,
+		"details": {
+			"player_card_data": player_card_data
+		},
+		"error": None,
+	}
 
 def multiple_player_cards(request):
 	ids = request.data.get('ids', [])
@@ -110,5 +121,29 @@ def get_profile_information(request):
 		form = ProfileForm()
 	
 	return render(request, 'laddr_site/get_profile.html', {'form': form})
+
+def get_team_member_ids(team_id):
+	if len(Team.objects.filter(id=team_id)) == 0:
+		return {"success": False, "details": None, "error": "No team with id: {0}".format(team_id)}
+	team = Team.objects.get(id=team_id)
+	member_ids = [x.id for x in team.members.all()]
+	details = {
+		"member_ids": member_ids
+	}
+	return {"success": True, "details": details, "error": None}
+
+def team_card_view(request):
+	team_id = request.data.get("team_id", None)
+	if team_id == None:
+		return {
+			"details": None,
+			"error": "Expected team_id in request, got None instead",
+			"success": False,
+		}
+	data = get_team_member_ids(team_id)
+	if not data["success"]:
+		return data
+	player_ids = data['details']['member_ids']
+	player_cards = [get_player_card(pid) for pid in player_ids]
 
 
